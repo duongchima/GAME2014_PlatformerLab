48d88c70a0b7ef8598e14cf4bccd3338fedfb507
Duong-Chi Ma <ma.duongchi@gmail.com>
2022-11-21 00:06:07 -0500

    Added platforms and moving platforms

---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour
+{
+    public PlatformDirection dir;
+
+    [Header("Movement Properties")]
+    [Range(1.0f, 20.0f)]
+    public float horizontalDistance = 8.0f;
+    [Range(1.0f, 20.0f)]
+    public float horizontalSpeed = 3.0f;
+    [Range(1.0f, 20.0f)]
+    public float verticalDistance = 8.0f;
+    [Range(1.0f, 20.0f)]
+    public float verticalSpeed = 3.0f;
+
+    private Vector2 startPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move()
+    {
+        switch (dir)
+        {
+            case PlatformDirection.HORIZONTAL:
+                transform.position = new Vector2(Mathf.PingPong(horizontalSpeed * Time.time, horizontalDistance) + startPoint.x, startPoint.y);
+                break;
+            case PlatformDirection.VERTICAL:
+                transform.position = new Vector2(startPoint.x, Mathf.PingPong(verticalSpeed * Time.time, verticalDistance) + startPoint.y);
+                break;
+            case PlatformDirection.DIAGONAL_UP:
+                transform.position = new Vector2(Mathf.PingPong(horizontalSpeed * Time.time, horizontalDistance) + startPoint.x, 
+                    Mathf.PingPong(verticalSpeed * Time.time, verticalDistance) + startPoint.y);
+                break;
+            case PlatformDirection.DIAGONAL_DOWN:
+                transform.position = new Vector2(Mathf.PingPong(horizontalSpeed * Time.time, horizontalDistance) + startPoint.x, 
+                    startPoint.y - Mathf.PingPong(verticalSpeed * Time.time, verticalDistance));
+                break;
+            case PlatformDirection.CUSTOM:
+                break;
+        }
+    }
+}
