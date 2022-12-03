aa209006290ea85634ba7b5493b051be30921d8f
Duong-Chi Ma <ma.duongchi@gmail.com>
2022-12-02 22:37:23 -0500

    added onscreen buttons

--- Assets/Scripts/Scene/GameController.cs
+++ Assets/Scripts/Scene/GameController.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject onScreenControls;
-
+    public GameObject miniMap;
     // Start is called before the first frame update
     void Awake()
     {
         onScreenControls = GameObject.Find("OnScreenControls");
         onScreenControls.SetActive(Application.isMobilePlatform);
         //FindObjectOfType<SoundManager>().PlayMusic(Sound.MAIN_MUSIC);
+
+        miniMap = GameObject.Find("MiniMap");
+        if (miniMap)
+        {
+            miniMap.SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        if ((miniMap) && (Input.GetKeyDown(KeyCode.M)))
+        {
+                miniMap.SetActive(!miniMap.activeInHierarchy);
+        }
     }
 }
